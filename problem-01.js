035ce1e2c2b36d59870c161cdb3b14371a0e4619
soniya-builds <soniyasmiles05@gmail.com>
2026-07-30 11:37:39 +0600

    Problem 01 solved

---
+++ problem-01.js
@@ -0,0 +1,20 @@
+function matchWinner(teamAGoals,teamBGoals){
+    if(typeof teamAGoals !=="number" || typeof teamBGoals !=="number"){
+        return "Invalid";
+    }
+    if (teamAGoals>teamBGoals){
+        return "Team A Won";
+    }
+    if (teamBGoals>teamAGoals) {
+        return "Team B Won";
+    }
+    else{
+    return "Draw";
+    }
+}
+
+// console.log(matchWinner(2,1)); 
+// console.log(matchWinner(1,3)); 
+// console.log(matchWinner(2,2));
+// console.log(matchWinner("3",2));
+// console.log(matchWinner("soniya",2));
