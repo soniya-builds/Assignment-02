13a2dd96dc78551a0d860daff6efacdc717f809c
soniya-builds <soniyasmiles05@gmail.com>
2026-07-30 22:39:11 +0600

    Problem 04 solved

--- problem-04.js
+++ problem-04.js
@@ -1,31 +1,22 @@
 function topRatedRestaurant(restaurants) {
-
-    if (!Array.isArray(restaurants) || restaurants.length === 0) {
+    if (!Array.isArray(restaurants)||restaurants.length===0){
         return "Invalid";
     }
-
     let topRestaurant = restaurants[0];
-
-    for (let i = 1; i < restaurants.length; i++) {
+    for (let i=1; i<restaurants.length; i++) {
         if (restaurants[i].rating > topRestaurant.rating) {
             topRestaurant = restaurants[i];
         }
     }
-
     return topRestaurant.name.toUpperCase();
 }
-
-// Test Cases
-console.log(topRatedRestaurant([
-    { name: "Chillox", rating: 4.5 },
-    { name: "Sultan's Dine", rating: 4.8 }
-])); // SULTAN'S DINE
-
-console.log(topRatedRestaurant([
-    { name: "KFC", rating: 4.2 },
-    { name: "Pizza Hut", rating: 4.6 }
-])); // PIZZA HUT
-
-console.log(topRatedRestaurant([])); // Invalid
-
-console.log(topRatedRestaurant("restaurants")); // Invalid
+// console.log(topRatedRestaurant([
+//     { name:"Chillox",rating:4.5},
+//     { name:"Sultan's Dine",rating:4.8}
+// ]));
+// console.log(topRatedRestaurant([
+//     {name:"KFC",rating:4.2},
+//     {name:"Pizza Hut",rating:4.6}
+// ]));
+// console.log(topRatedRestaurant([]));
+// console.log(topRatedRestaurant("restaurants"));
