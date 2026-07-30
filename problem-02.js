d451ed5d50389d19c1a22f35e05f99b78cd01233
soniya-builds <soniyasmiles05@gmail.com>
2026-07-30 21:53:19 +0600

    Problem 02 solved

--- problem-02.js
+++ problem-02.js
@@ -1,9 +1,21 @@
 //greater than a specific value 
 
 function isElevatorSafe(weights){
-    if(!Array.isArray){
+    if(!Array.isArray(weights)){
         return "Invalid";
     }
-    
-
+    let allWeight = 0;
+    for(let i=0;i<weights.length;i++){
+        allWeight+=weights[i];                               //allWeight+Array[i];
+    }
+    if(allWeight<=400){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
+// console.log(isElevatorSafe([60, 75, 50]));
+// console.log(isElevatorSafe([90, 100, 95, 120]));
+// console.log(isElevatorSafe([400]));
+// console.log(isElevatorSafe("60,75,50")); 
